0e8460180ea68e1d90e230dd8c0625747b8e94ab
Peter Liu <90960163+alphadeathx@users.noreply.github.com>
2024-01-21 19:42:23 +0800

    Add files via upload
    first commit

---
+++ frontend/src/user/components/UsersList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+import "./UsersList.css";
+
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>找不到該名使用者.</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places.length}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
